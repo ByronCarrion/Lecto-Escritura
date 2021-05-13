91de617bcf7c7a3a1ab13ada9ea81f279d0ae16d
ByronCarrion <carrionmejia756@gmail.com>
2021-05-13 08:58:30 -0600

    [fix(Login and controller) se creo login para el usuario]

---
+++ app/src/main/java/edu/lectoescritura/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package edu.lectoescritura.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+        val userId: String,
+        val displayName: String
+)
